9fee246d4e75bca6741638834a55e37bde023820
Shubkin1isp22 <163159716+Shubkin1isp22@users.noreply.github.com>
2026-03-14 21:58:05 +0700

    JWT key убран в .env

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ load_dotenv()
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
-    app.config["JWT_SECRET_KEY"] = "super-secret-key"
 
     db.init_app(app)
     migrate.init_app(app, db)
